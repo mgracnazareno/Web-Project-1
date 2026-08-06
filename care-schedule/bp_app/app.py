b4e00b769511591afbe0ae72ac3fde3867634d3e
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-08-05 20:19:10 -0400

    restructure app into bp_app directory

--- care-schedule/bp_app/app.py
+++ care-schedule/bp_app/app.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from flask import (Flask)
+from flask import (Flask, jsonify, render_template, request)
+
+from .models import db
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,5 +11,9 @@ app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///appointments.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
