bf8b22cb41c169954286893296e4360c5508f55d
Mary Grace Nazareno <mgracenazareno@gmail.com>
2026-08-07 11:42:56 -0400

    add route for registering professional

--- care-schedule/bp_app/professional_routes.py
+++ care-schedule/bp_app/professional_routes.py
@@ -1,9 +1,11 @@
 
 import os
 from dotenv import load_dotenv
-from utils import validate_registration
+
+from bp_app.models import db, Professional
+from utils import validate_registration, validate_professional_registration
 from flask_login import current_user
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, flash, render_template, redirect, url_for, request
 
 professional = Blueprint("professional", __name__)
 
@@ -26,5 +28,39 @@ def register():
         specialty = request.form['specialty'].strip()
         bio = request.form['biography'].strip()
 
-        errors = validate_registration(username, email, password)
-    return None
+        errors = validate_professional_registration(username, email, password, firstname, lastname, specialty, bio)
+
+        if errors:
+            for error in errors:
+                flash(error, "danger")
+                # re-render the form, keepint what the user typed (except password)
+                return render_template(
+                    "professional/register.html",
+                    username=username,
+                    email=email,
+                    firstname=firstname,
+                    lastname=lastname,
+                    specialty=specialty,
+                    bio=bio,
+                )
+
+            # No errors - create and save the Professional
+            professional_user = Professional(
+                username=username,
+                email=email,
+                firstname=firstname,
+                lastname=lastname,
+                specialty=specialty,
+                biography=bio,
+            )
+
+            professional_user.set_password(password)
+
+            db.session.add(professional_user)
+            db.session.commit()
+
+            flash("Your account has been created!!!", "success")
+            return redirect(url_for("professional.login"))
+
+    # GET request
+    return render_template("professional/register.html")
